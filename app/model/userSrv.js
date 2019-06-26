02c6f94140b3d7ae80aeefd828cca801f0a2f9a0
nirch <nirh2@yahoo.com>
2019-06-26 11:37:00 +0300

    changing login to work with Parse

--- app/model/userSrv.js
+++ app/model/userSrv.js
@@ -1,13 +1,13 @@
 
-app.factory("userSrv", function($q, $http) {
+app.factory("userSrv", function ($q) {
 
     var activeUser = null; // new User({id: 1, fname: "Nir" ...})
 
-    function User(plainUser) {
-        this.id = plainUser.id;
-        this.fname = plainUser.fname;
-        this.lname = plainUser.lname;
-        this.email = plainUser.email;
+    function User(parseUser) {
+        this.id = parseUser.id;
+        this.fname = parseUser.get("fname");
+        this.lname = parseUser.get("lname");
+        this.email = parseUser.get("email");
     }
 
 
@@ -21,27 +21,17 @@ app.factory("userSrv", function($q, $http) {
         var async = $q.defer();
 
         activeUser = null;
-        $http.get("app/model/data/users.json").then(function(res) {
-            var users = res.data;
-            for (var i = 0; i < users.length && !activeUser; i++) {
-                if (email === users[i].email && pwd === users[i].pwd) {
-                    activeUser = new User(users[i]);
-                    async.resolve(activeUser);
-                } 
-            }
-            if (!activeUser) {
-                async.reject(401);
-            }
-        }, function(err) {
-            async.reject(err);
-        })
 
-        // if (email === "nir@nir.com" && pwd === "123") {
-        //     activeUser = new User({ id: 1, fname:"Nir", lname: "Channes", email: "nir@nir.com" });
-        //     async.resolve(activeUser);
-        // } else {
-        //     async.reject(401);
-        // }
+        // Pass the username and password to logIn function
+        Parse.User.logIn(email, pwd).then(function (user) {
+            // Do stuff after successful login
+            console.log('Logged in user', user);
+            activeUser = new User(user);
+            async.resolve(activeUser);
+        }).catch(error => {
+            console.error('Error while logging in user', error);
+            async.reject(error);
+        });
 
         return async.promise;
     }
